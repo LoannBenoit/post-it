f57c84dc0124322de1108fdc19b37fe28e899dab
loann BENOIT <loann.benoit@hotmail.com>
2019-12-03 11:20:03 +0100

    Add VueJS and Electron to the project

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'post-it'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
